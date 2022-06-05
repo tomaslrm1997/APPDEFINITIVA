fba6c6a72b5d6853222475f7a8186615026ad900
tomaslrm1997 <you@example.com>
2022-06-05 01:20:40 -0300

    Comienzo de agregado de estilo, finalización de funcionalidades

--- js/javascript.js
+++ js/javascript.js
@@ -52,9 +52,10 @@ const objGastos= {
 
 gastos.push (objGastos);
 
-mostrarHTML();
+mostrarGasto();
 }
-function mostrarHTML() {
+function mostrarGasto() {
+    limpiarHTML();
 gastos.forEach(gasto => {
 
     const resultados = document.querySelector("#resultado");
@@ -71,5 +72,52 @@ resultados.appendChild(nuevoli);
 
     
 });
-gastos = [];
+mostrartotal (gastos);
+}
+function mostrartotal (gasto) {
+let total= 0;
+let cantidad=0;
+
+gasto.forEach (objgasto => {
+    total += objgasto.numero;
+    cantidad++;
+});
+
+
+
+mostrarHtml (total,cantidad);
+}
+
+function mostrarHtml (total,cantidad) {
+ const resultado=document.querySelector ("#resultado");
+ const mostrartotal= document.createElement("div");
+ mostrartotal.classList.add("mostrartotal");
+ mostrartotal.textContent="Total:";
+ 
+ const TotalSpan= document.createElement("span");
+ TotalSpan.classList.add ("parrafoSpan");
+ TotalSpan.textContent=`$${total}`;
+
+
+ const mostrarPago=document.createElement("div");
+ mostrarPago.classList.add ("mostrarTotales");
+ mostrarPago.textContent="A cada uno le toca aportar: ";
+
+ const pagoMostrar= document.createElement("span");
+ pagoMostrar.classList.add ("parrafoSpan");
+ pagoMostrar.textContent = `$${total/cantidad}`;
+
+ mostrartotal.appendChild(TotalSpan); 
+ mostrarPago.appendChild(pagoMostrar);
+
+ resultado.appendChild(mostrartotal);
+ resultado.appendChild(mostrarPago);
+
+
+}
+function limpiarHTML() {
+    const resultado=document.querySelector("#resultado");
+    while (resultado.firstChild) {
+        resultado.removeChild(resultado.firstChild);
+    }
 }
